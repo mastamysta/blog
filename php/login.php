64bd4567d5e58d5bfc83115da81d73330da08cd9
mastamysta <benjamin-read@hotmail.co.uk>
2020-05-27 13:39:23 +0100

    Added mysqli connection method

--- php/login.php
+++ php/login.php
@@ -1,6 +1,20 @@
 <?php
     $USERNAME = $_POST["userName"];
     $PASSWORD = $_POST["passWord"];
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
-    echo($USERNAME . " " . $PASSWORD);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    echo("memes");
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
 ?>
