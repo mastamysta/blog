8243ca6450be55377906e7cbdd86689e9b4a60a5
mastamysta <benjamin-read@hotmail.co.uk>
2020-05-27 14:30:08 +0100

    Added mysqli connection to mysql db

--- php/login.php
+++ php/login.php
@@ -2,19 +2,21 @@
     $USERNAME = $_POST["userName"];
     $PASSWORD = $_POST["passWord"];
     
-    $servername = "localhost";
+    $servername = "localhost:3308";
     $username = "root";
     $password = "";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password);
 
-    echo("memes");
 
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    echo "Connected successfully\n";
+
+    mysqli_close($conn);
+    echo "Session Closed\n";
 
 ?>
