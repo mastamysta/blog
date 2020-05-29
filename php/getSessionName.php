702d2c85c0c341e768d5af328abc71d2e718b903
mastamysta <benjamin-read@hotmail.co.uk>
2020-05-29 12:44:44 +0100

    Created logout and session method files

---
+++ php/getSessionName.php
@@ -0,0 +1,40 @@
+<?php
+
+function getSessionName($email){
+    //connection parameters
+    $servername = "localhost:3308";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbName = "adminaccounts";
+
+    // Create connection
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbName);
+    mysqli_set_charset($conn, 'utf8');
+
+    //Create query
+    $sql = mysqli_prepare($conn, "SELECT Username FROM admin WHERE Email= ?");
+    mysqli_stmt_bind_param($sql, "s", $email);
+    $userNameResult;
+
+    //Execute query
+    mysqli_stmt_execute($sql);
+    mysqli_stmt_bind_result($sql, $userNameResult);
+
+    //Count results
+    $count = 0;
+    while(mysqli_stmt_fetch($sql)){
+        $count += 1;
+    }
+
+    if($count == 1){
+        mysqli_stmt_close($sql);
+        mysqli_close($conn);
+        return $userNameResult;
+    }else{
+        mysqli_stmt_close($sql);
+        mysqli_close($conn);
+        die("A session error was encountered");
+    }
+}
+
+?>
