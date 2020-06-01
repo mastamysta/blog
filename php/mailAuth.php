0a5f49ae6ec318f6070c91c6805ff6e3f84e082c
mastamysta <benjamin-read@hotmail.co.uk>
2020-06-01 15:54:12 +0100

    Added autofill for link followers on verification email

--- php/mailAuth.php
+++ php/mailAuth.php
@@ -5,53 +5,55 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require '..\composer\vendor\autoload.php';
 
+function authenticate($EMAIL, $USERNAME, $KEY){
+      $mail = new PHPMailer(TRUE);
+   try {
 
-$mail = new PHPMailer(TRUE);
-try {
-
-   //this needs a patch to increase security
-   $mail->SMTPOptions = array(
-      'ssl' => array(
-          'verify_peer' => false,
-          'verify_peer_name' => false,
-          'allow_self_signed' => true
-      )
-   );
+      //this needs a patch to increase security
+      $mail->SMTPOptions = array(
+         'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+         )
+      );
+      
+      $mail->setFrom('webmaster@benread.dev', 'DO NOT REPLY -- USER AUTHENTICATION');
+      $mail->addAddress($EMAIL, 'Site User');
+      $mail->Subject = 'Verify Your Account';
+      $mail->Body = 'This is your authentication key for the site: ' . $KEY . "<br>" . "Use this auth link: http://blog/views/verify.php?key=$KEY&email=$EMAIL";
+      
+      /* SMTP parameters. */
+      
+      /* Tells PHPMailer to use SMTP. */
+      $mail->isSMTP();
+      
+      /* SMTP server address. */
+      $mail->Host = 'mail.benread.dev';
    
-    $mail->setFrom('webmaster@benread.dev', 'Darth Vader');
-    $mail->addAddress('benjamin-read@hotmail.co.uk', 'Emperor');
-    $mail->Subject = 'Force';
-    $mail->Body = 'There is a great disturbance in the Force.';
-    
-    /* SMTP parameters. */
-    
-    /* Tells PHPMailer to use SMTP. */
-    $mail->isSMTP();
-    
-    /* SMTP server address. */
-    $mail->Host = 'mail.benread.dev';
- 
-    /* Use SMTP authentication. */
-    $mail->SMTPAuth = TRUE;
-    
-    /* Set the encryption system. */
-    $mail->SMTPSecure = 'tls';
-    
-    /* SMTP authentication username. */
-    $mail->Username = 'webmaster@benread.dev';
-    
-    /* SMTP authentication password. */
-    $mail->Password = 'XLuEM(b-Uhwe';
-    
-    /* Set the SMTP port. */
-    $mail->Port = 26;
+      /* Use SMTP authentication. */
+      $mail->SMTPAuth = TRUE;
+      
+      /* Set the encryption system. */
+      $mail->SMTPSecure = 'tls';
+      
+      /* SMTP authentication username. */
+      $mail->Username = 'webmaster@benread.dev';
+      
+      /* SMTP authentication password. */
+      $mail->Password = 'XLuEM(b-Uhwe';
+      
+      /* Set the SMTP port. */
+      $mail->Port = 26;
 
-    $mail->SMTPDebug = 4; 
-    
-    /* Finally send the mail. */
-    $mail->send();
- }catch (Exception $e){
-    /* PHPMailer exception. */
-    echo $e->errorMessage();
- }
+      $mail->SMTPDebug = 4; 
+      
+      /* Finally send the mail. */
+      $mail->send();
+   }catch (Exception $e){
+      /* PHPMailer exception. */
+      echo $e->errorMessage();
+   }
+}
+
 ?>
